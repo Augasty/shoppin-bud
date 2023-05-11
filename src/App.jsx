e96535ae0ba7e3f748b24f9153e80bc25c89e54d
Augasty <sayaksengupta140@gmail.com>
2023-05-11 21:16:34 +0530

    first actual commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import React, {useState,useEffect} from 'react'
+import List from './List'
+import Alert from './Alert'
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+
+  const [isEditing,setIsEditing] = useState(false)
+  const [editId,setEditId] = useState(null)
+  const [alert,setAlert] = useState({show:true ,msg:'',type:''})
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log('hello')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <section className='section-center'>
+    <form className='grocery-form' onSubmit={handleSubmit}>
+      {alert.show && <Alert/>}
+      <h3>Shoppin-bud</h3>
+
+      <div className='form-control'>
+        <input type='text' className='grocery' placeholder='eg. 4 eggs' value={name} onChange={(e)=>setName(e.target.value)}></input>
+        <button type='submit' className='submit-btn'>
+          {isEditing? 'edit':'submit'}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </form>
+      <div className='grocery-container'>
+        <List/>
+        <button className='clear-btn'>Clear items</button>
+      </div>
+    </section>
+    )
 }
 
 export default App
