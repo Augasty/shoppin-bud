1aec38ef30ba07ddd699b7e5ea7e17ee2c99d781
Augasty <sayaksengupta140@gmail.com>
2023-05-13 09:42:33 +0530

    done

--- src/List.jsx
+++ src/List.jsx
@@ -10,7 +10,7 @@ const List = ({ items , remove , edit}) => {
 
         return (<article key={id} className='grocery-item'>
           
-          <div style={{color:'pink'}}>
+          <div style={{}}>
           <p className='title'>{title}</p>
             <button type='button' style={{ color:'greenyellow'}} onClick={()=>edit(id)}><FaEdit /></button>
             <button type='button' style={{ color:'red'}} onClick={()=>remove(id)}><FaTrash /></button>
